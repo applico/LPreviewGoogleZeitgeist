901214d6af822450dafa3e2db58c44d4609f5dc8
Matt Powers <mtpowers@gmail.com>
2014-08-22 16:36:44 -0400

    Added some base drawer activities, started to play around with the fab, almost done with the globals screen, added in the viewpageradapter and the recyclerview.

---
+++ app/src/main/java/applico/googlezlpreview/activities/FabTestDriver.java
@@ -0,0 +1,36 @@
+package applico.googlezlpreview.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import applico.googlezlpreview.R;
+
+public class FabTestDriver extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab_test_driver);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.fab_test_driver, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
